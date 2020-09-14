61825a59510acb551867dca453138c6b3d7f741d
Sotirios M. Roussis <xtonousou@gmail.com>
2020-09-15 02:00:36 +0300

    Removed duplicate line of copy

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ command chmod +x /opt/space/*.sh
 command yes | command cp Check-MySQL-Health.timer /etc/systemd/system/Check-MySQL-Health.timer
 command yes | command cp Check-MySQL-Health.service /etc/systemd/system/Check-MySQL-Health.service
 command yes | command cp FortiADC-MySQL-Healthcheck-Server.service /etc/systemd/system/FortiADC-MySQL-Healthcheck-Server.service
-command yes | command cp FortiADC-MySQL-Healthcheck-Server.service /etc/systemd/system/FortiADC-MySQL-Healthcheck-Server.service
 
 command systemctl daemon-reload
 command systemctl enable Check-MySQL-Health.timer
